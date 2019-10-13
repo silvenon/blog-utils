a17ed21a8906b8719b4db046a80a40e420fb85e2
Matija Marohnić <matija.marohnic@gmail.com>
2019-10-13 18:20:11 +0200

    Add missing tests

---
+++ remark-remove-frontmatter.test.js
@@ -0,0 +1,21 @@
+const unified = require('unified')
+const parse = require('remark-parse')
+const frontmatter = require('remark-frontmatter')
+const removeFrontmatter = require('./remark-remove-frontmatter')
+const stringify = require('remark-stringify')
+const { getFileMd } = require('./test/utils')
+
+const processor = unified()
+  .use(parse)
+  .use(frontmatter)
+  .use(removeFrontmatter)
+  .use(stringify)
+
+it('removes frontmatter', done => {
+  expect.hasAssertions()
+  processor.process(getFileMd(), (err, file) => {
+    if (err != null) return done(err)
+    expect(String(file).trim()).toMatchSnapshot()
+    done()
+  })
+})
---
+++ remark-save-frontmatter.test.js
@@ -0,0 +1,30 @@
+const unified = require('unified')
+const parse = require('remark-parse')
+const frontmatter = require('remark-frontmatter')
+const saveFrontmatter = require('./remark-save-frontmatter')
+const removeFrontmatter = require('./remark-remove-frontmatter')
+const stringify = require('retext-stringify')
+const { getFileMd } = require('./test/utils')
+
+const processor = unified()
+  .use(parse)
+  .use(frontmatter)
+  .use(saveFrontmatter)
+  .use(removeFrontmatter)
+  .use(stringify)
+
+it('saves parsed frontmatter', done => {
+  expect.hasAssertions()
+  processor.process(getFileMd(), (err, file) => {
+    if (err != null) return done(err)
+    expect(file.data).toMatchInlineSnapshot(`
+      Object {
+        "frontmatter": Object {
+          "author": "Charles Darwin",
+          "title": "On the Origin of Species",
+        },
+      }
+    `)
+    done()
+  })
+})
