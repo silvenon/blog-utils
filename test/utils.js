c567fb75c48e6b2a8e596d2ba3a9086254c3d72d
Matija Marohnić <matija.marohnic@gmail.com>
2020-04-18 19:55:13 +0200

    Format with Prettier

--- test/utils.js
+++ test/utils.js
@@ -3,17 +3,11 @@ const path = require('path')
 
 const getFileMd = () =>
   toVFile.readSync(
-    path.resolve(
-      __dirname,
-      '../__fixtures__/1859-11-24-post.md',
-    ),
+    path.resolve(__dirname, '../__fixtures__/1859-11-24-post.md'),
   )
 const getFileMdx = () =>
   toVFile.readSync(
-    path.resolve(
-      __dirname,
-      '../__fixtures__/1859-11-24-post.mdx',
-    ),
+    path.resolve(__dirname, '../__fixtures__/1859-11-24-post.mdx'),
   )
 
 module.exports = {
